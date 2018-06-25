30f7e317e16f2dd013621e730d2ecd76f3b2d2b2
HCKgit <hckilburn@gmail.com>
2018-06-24 22:15:41 -0300

    minor edits

--- src/main/kotlin/kotlindata/controller/ArticleController.kt
+++ src/main/kotlin/kotlindata/controller/ArticleController.kt
@@ -5,7 +5,7 @@ import kotlindata.repository.ArticleRepository
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
-import java.util.*
+//import java.util.*
 import javax.validation.Valid
 
 @RestController
