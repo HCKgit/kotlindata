edfaf10cba333116c03604050dd0ee230d76abab
HCKgit <hckilburn@gmail.com>
2018-06-24 21:31:51 -0300

    initial commit

---
+++ src/main/kotlin/kotlindata/SbKotdataApplication.kt
@@ -0,0 +1,11 @@
+package kotlindata
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SbKotdataApplication
+
+fun main(args: Array<String>) {
+    runApplication<SbKotdataApplication>(*args)
+}
---
+++ src/test/kotlin/kotlindata/SbKotdataApplicationTests.kt
@@ -0,0 +1,16 @@
+package kotlindata
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class SbKotdataApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
